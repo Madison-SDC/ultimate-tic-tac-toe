0f8240c1cc99fa743e246d56b8b2df386deaf96b
Mark Wiemer <mwwiemer@outlook.com>
2017-03-25 15:29:31 -0500

    Haha actually added the new files I made whoopsie daisy. Anyway yeah undo functionality is officially in so that's cool

---
+++ Assets/Resources/Scripts/Move.cs
@@ -0,0 +1,50 @@
+﻿public class Move
+{
+    private Board board;
+    private BoardSpot spot;
+
+    /// <summary>
+    /// The active board at the time of this move
+    /// </summary>
+    public Board Board
+    {
+        get
+        {
+            return board;
+        }
+
+        set
+        {
+            board = value;
+        }
+    }
+
+    /// <summary>
+    /// The local spot this move was made on
+    /// </summary>
+    public BoardSpot Spot
+    {
+        get
+        {
+            return spot;
+        }
+
+        set
+        {
+            spot = value;
+        }
+    }
+    
+    /// <summary>
+    /// A new move on board b at position s for player p
+    /// </summary>
+    /// <param name="b"></param>
+    /// <param name="s"></param>
+    /// <param name="p"></param>
+    public Move(Board b, BoardSpot s)
+    {
+        Board = b;
+        Spot = s;
+    }
+
+}
---
+++ Assets/Resources/Scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player {
+
+    private int turn;
+    private Color color;
+    private Sprite sprite;
+
+    public int Turn { get { return turn; } set { turn = value; } }
+
+    public Color Color { get { return color; } set { color = value; } }
+
+    public Sprite Sprite { get { return sprite; }  set { sprite = value; } }
+
+    public Player(int turn, Color color, Sprite sprite)
+    {
+        Turn = turn;
+        Color = color;
+        Sprite = sprite;
+    }
+}
