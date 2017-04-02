31e9a21642b1b724b8699a46d97275e1bf531491
Mark Wiemer <mwwiemer@outlook.com>
2017-04-01 23:57:49 -0500

    Removed more unnecessary logic on reset button

--- Assets/Resources/Scripts/ResetButton.cs
+++ Assets/Resources/Scripts/ResetButton.cs
@@ -3,30 +3,16 @@ using UnityEngine.UI;
 
 public class ResetButton : Button {
 
-    private bool isResetting;
-    /// <summary>
-    /// Delay in seconds between removals
-    /// </summary>
-    private const float TIME = 0.1f;
-    private float timer;
-
     private Game game;
-
-    /// <summary>
-    /// Summary
-    /// </summary>
-    public Game Game { get { return game; } }
 
     protected override void Start()
     {
-        isResetting = false;
-        timer = TIME;
         game = GameObject.Find("Global Board").GetComponent<Game>();
     }
 
     // Update is called once per frame
     void Update ()
     {
-        interactable = !Game.IsClear;
+        interactable = !game.IsClear;
 	}
 }
