ec5543c4f23dbf203538cdf4ea5ace3642454f7a
Mark Wiemer <mwwiemer@outlook.com>
2017-04-02 00:40:24 -0500

    Added redo button

---
+++ Assets/Resources/Scripts/RedoButton.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class RedoButton : Button {
+
+    Game game;
+
+	// Use this for initialization
+	protected override void Start () {
+        game = GameObject.Find("Global Board").GetComponent<Game>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        interactable = game.CanRedo();
+	}
+}
