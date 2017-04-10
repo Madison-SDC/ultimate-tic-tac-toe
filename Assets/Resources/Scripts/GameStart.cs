666d3c7355511d2daf69e6cb55eaab6d45bdc6ac
Mark Wiemer <mwwiemer@outlook.com>
2017-04-09 20:29:04 -0500

    Restructure initialization based on game mode

---
+++ Assets/Resources/Scripts/GameStart.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameStart : MonoBehaviour {
+    static int gameMode = ONE_PLAYER;
+
+    const int ONE_PLAYER = 1;
+    const int TWO_PLAYER = 2;
+
+    /// <summary>
+    /// The game mode of the current game
+    /// </summary>
+    public static int GameMode
+    {
+        get { return gameMode; }
+        set
+        {
+            if(value == ONE_PLAYER || value == TWO_PLAYER)
+            {
+                gameMode = value;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Add the game to the global board and then destroy this (no longer necessary)
+    /// </summary>
+    private void Awake()
+    {
+        if(GameMode == ONE_PLAYER)
+        {
+            gameObject.AddComponent<SinglePlayerGame>();
+        }
+        else if (GameMode == TWO_PLAYER)
+        {
+            gameObject.AddComponent<Game>();
+        }
+        Destroy(this);
+    }
+}
