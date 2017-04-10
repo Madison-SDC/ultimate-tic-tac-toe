a501fe9dff65751d54a7d50c4ba7f7874c3e5504
Mark Wiemer <mwwiemer@outlook.com>
2017-04-09 20:30:13 -0500

    Start game in specified game mode using StartGame buttons on Menu Scene

---
+++ Assets/Resources/Scripts/StartGameButton.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGameButton : MonoBehaviour {
+
+    public int gameMode;
+
+	public void OnClick()
+    {
+        GameStart.GameMode = gameMode;
+        SceneManager.LoadScene(1); // game scene
+    }
+}
