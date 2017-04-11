d85429525d1d93b1d86f910f5d314bbb9aedb9e3
Mark Wiemer <mwwiemer@outlook.com>
2017-04-10 20:56:57 -0500

    Reformat StartGameButton for readability

--- Assets/Resources/Scripts/StartGameButton.cs
+++ Assets/Resources/Scripts/StartGameButton.cs
@@ -1,11 +1,12 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class StartGameButton : MonoBehaviour {
+public class StartGameButton : MonoBehaviour
+{
 
     public int gameMode;
 
-	public void OnClick()
+    public void OnClick()
     {
         GameStart.GameMode = gameMode;
         SceneManager.LoadScene(1); // game scene
