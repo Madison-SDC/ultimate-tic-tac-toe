7bb297f63ab2694e990c130e529547883dc42bfc
Mark Wiemer <mwwiemer@outlook.com>
2017-05-23 11:16:31 -0500

    Make more abstract Test.cs

--- Assets/Resources/Scripts/Test.cs
+++ Assets/Resources/Scripts/Test.cs
@@ -2,9 +2,7 @@
 using System.Collections.Generic;
 
 public class Test : UnityEngine.UI.Button {
-
-    static Game TestGame() { return Game.CurrentGame; }
-
+    
 	public void AllTests()
     {
         PlaySimpleGame();
@@ -45,29 +43,52 @@ public class Test : UnityEngine.UI.Button {
 
             args.Add(new int[2] { 1, 2 }); // x wins
         }
-        TestGame().Reset();
 
-        int i = 0;
-        int spotRow = args[i][0];
-        int spotCol = args[i][1];
-        int boardRow = args[i][2];
-        int boardCol = args[i][3];
+        TestGame(args, "Simple game passed", "Simple game failed");
+    }
 
-        bool val = Play(spotRow, spotCol, boardRow, boardCol);
+    static bool TestGame(List<int[]> args, string success, string fail)
+    {
+        Game.CurrentGame.Reset();
 
-        while (val) // last move worked
+        int i = -1;
+        int[] firstMoveArgs = args[0];
+        int spotRow = firstMoveArgs[0];
+        int spotCol = firstMoveArgs[1];
+        int boardRow = firstMoveArgs[2];
+        int boardCol = firstMoveArgs[3];
+        bool testPass = true;
+
+        while (testPass) // last move worked
         {
             // play next move
             i++;
-            
+
             // all moves played
             if (args.Count <= i) { break; }
 
+            int[] nextMoveArgs = args[i];
             spotRow = args[i][0];
             spotCol = args[i][1];
-            val = Play(spotRow, spotCol);
+            if(nextMoveArgs.Length > 2)
+            {
+                // play on specific board
+                boardRow = nextMoveArgs[2];
+                boardCol = nextMoveArgs[3];
+            }
+            else
+            {
+                // play on active board
+                boardRow = -1;
+                boardCol = -1;
+            }
+            
+            testPass = Play(spotRow, spotCol, boardRow, boardCol);
         }
-        if(val) { Debug.Log("Simple game test passed"); }
+
+        Debug.Log(testPass ? success : fail);
+
+        return testPass;
     }
 
     /// <summary>
@@ -87,7 +108,7 @@ public class Test : UnityEngine.UI.Button {
         int boardCol = -1
         )
     {
-        Game game = TestGame();
+        Game game = Game.CurrentGame;
 
         // can't play on a finished game
         if (!GameOverMatch(game, false)) { return false; };
@@ -103,7 +124,7 @@ public class Test : UnityEngine.UI.Button {
         Spot spot = localBoard.Get(new Location(spotRow, spotCol));
 
         // spot must be empty to play
-        if (IsEmpty(spot)) { game.UpdateDisplay(spot); }
+        if (OwnerIs(spot, null)) { game.PreviewMove(spot); }
         else { return false; }
 
         game.Confirm();
@@ -116,11 +137,20 @@ public class Test : UnityEngine.UI.Button {
     /// </summary>
     /// <param name="spot"></param>
     /// <returns></returns>
-    static bool IsEmpty(Spot spot)
+    static bool OwnerIs(Spot spot, Player owner)
     {
-        if(spot.Owner != null)
+        if(spot.Owner != owner)
         {
-            Debug.Log("Spot not empty!");
+            if (!(spot is Board))
+            {
+                Debug.Log("Spot (" + spot.Loc +
+                    ") on board  (" + spot.ParentBoard.Loc +
+                    ")not owned by " + owner.Name + "!");
+            } else
+            {
+                Debug.Log("Board (" + spot.Loc +
+                    ")not owned by " + owner.Name + "!");
+            }
             return false;
         }
         return true;
