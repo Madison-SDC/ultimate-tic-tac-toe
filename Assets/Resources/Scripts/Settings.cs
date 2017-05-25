3e9748d34836c56a25972d349cb506410225e77d
Mark Wiemer <mwwiemer@outlook.com>
2017-05-25 12:47:23 -0500

    Architecture rewrite: use event-based programming to decouple logic and UI layers

--- Assets/Resources/Scripts/Settings.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class Settings {
-    public static Player
-        p1 = new Player(1, Color.red, Resources.Load<Sprite>("Sprites/x"), "P1"),
-        p2 = new Player(2, Color.blue, Resources.Load<Sprite>("Sprites/o"), "P2");
-}
