9c9a50e7e1b654b3cb099050e857011677653834
Mark Wiemer <mwwiemer@outlook.com>
2017-06-11 22:49:54 -0500

    Add Button controller for game flow buttons

---
+++ Assets/Resources/Scripts/Buttons/ButtonEventArgs.cs
@@ -0,0 +1,9 @@
+﻿public class ButtonEventArgs : System.EventArgs
+{
+    public bool value;
+
+	public ButtonEventArgs(bool value)
+    {
+        this.value = value;
+    }
+}
---
+++ Assets/Resources/Scripts/Buttons/ToggledButton.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine.UI;
+
+public class ToggledButton : Button
+{
+	void HandleCanConfirmChanged(object o, ButtonEventArgs e)
+    {
+        enabled = e.value;
+    }
+}
